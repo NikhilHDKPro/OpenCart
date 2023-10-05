94934161feb89bb0755d73edfd5139273475c905
NikhilHDK <nikhilhadke9@gmail.com>
2023-10-05 23:05:23 +0530

    [NikhilhdkPro] Tcase Updated

--- src/test/java/testsOn_OC/TCase1.java
+++ src/test/java/testsOn_OC/TCase1.java
@@ -5,12 +5,14 @@ import org.testng.annotations.Test;
 import ocBase.Base;
 import ocPageEvents.HomePageEvents;
 import ocPageEvents.LoginPageEvents;
+import ocPageEvents.RegistrationPageEvents;
 import ocUtilities.ElementFetch;
 
-public class TC_LoginFunctionality extends Base {
+public class TCase1 extends Base {
 	ElementFetch ef = new ElementFetch();
 	HomePageEvents hp = new HomePageEvents();
 	LoginPageEvents lp = new LoginPageEvents();
+	RegistrationPageEvents rp = new RegistrationPageEvents();
 
 	@Test
 	public void LoginToOC() {
@@ -18,4 +20,10 @@ public class TC_LoginFunctionality extends Base {
 		lp.varifyIfLoginPageIsLoaded();
 		lp.EnterCredentialsAndLogin();
 	}
+
+	@Test
+	public void Registration() {
+		hp.GoToRegistrationPage();
+		rp.EnterRegistrationCredentials();
+		}
 }
